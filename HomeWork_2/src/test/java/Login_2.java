f45eff48dd49f5ebca6a014bb409b78df221ac58
Eduard-QA <eduard.mickel@gmail.com>
2022-12-18 22:25:58 +0200

    HW_2_18.12

--- HomeWork_2/src/test/java/Login_2.java
+++ HomeWork_2/src/test/java/Login_2.java
@@ -21,14 +21,57 @@ WebDriver wd;
         WebElement email = wd.findElement(By.name("email"));
         email.click();
         email.clear();
-        email.sendKeys("rehovot12@mail.ru");
+        email.sendKeys("rehovot13@mail.ru");
         WebElement password = wd.findElement(By.name("password"));
         password.click();
         password.clear();
         password.sendKeys("Remember123@");
         WebElement registration = wd.findElement(By.name("registration"));
         registration.click();
-        
+        wd.quit();
+
+    }
+    @Test
+    public void loginSuccess (){
+    wd.get("https://telranedu.web.app/home");
+    WebElement itemLogin = wd.findElement(By.linkText("LOGIN"));
+    itemLogin.click();
+    WebElement email = wd.findElement(By.name("email"));
+    email.click();
+    email.clear();
+    email.sendKeys("rehovot@mail.ru");
+    WebElement password = wd.findElement(By.name("password"));
+    password.click();
+    password.clear();
+    password.sendKeys("Remember123@");
+    WebElement button = wd.findElement(By.name("login"));
+    button.click();
+    wd.quit();
+    }
+    @Test
+    public void registrIlcaro (){
+    wd.get("https://ilcarro-1578153671498.web.app/search");
+    WebElement singup = wd.findElement(By.partialLinkText("Sign"));
+    singup.click();
+    WebElement name = wd.findElement(By.id("name"));
+    name.click();
+    name.clear();
+    name.sendKeys("Dov");
+    WebElement lastname = wd.findElement(By.id("lastName"));
+    lastname.click();
+    lastname.clear();
+    lastname.sendKeys("Pooh");
+    WebElement email = wd.findElement(By.id("email"));
+    email.click();
+    email.clear();
+    email.sendKeys("rehovot1@mail.ru");
+    WebElement password = wd.findElement(By.id("password"));
+    password.click();
+    password.clear();
+    password.sendKeys("Remember123@");
+    WebElement chek = wd.findElement(By.className("checkbox-container"));
+    chek.click();
+        wd.quit();
 
 
     }
